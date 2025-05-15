69ff00372fb1d282ad8ae53b6f14b0c85856c1c2
Pulkit <pulkitdadheech83@gmail.com>
2025-05-15 11:55:26 +0530

    made a sample calculator

---
+++ new.js
@@ -0,0 +1,17 @@
+// Create a sample calculator
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+
+
+// Example usage
+console.log("Addition: ", add(5, 3)); // 8
+console.log("Subtraction: ", subtract(5, 3)); // 2
+// console.log("Multiplication: ", multiply(5, 3)); // 15
+// console.log("Division: ", divide(5, 3)); // 1.666...
